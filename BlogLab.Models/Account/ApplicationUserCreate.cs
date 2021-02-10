cd7e59143c5dcedb6cc8c121442269cf5a79dd6e
Nikhil-Dev-Hack <75464384+Nikhil-Dev-Hack@users.noreply.github.com>
2021-02-10 17:57:35 +0530

    Account Model is added with some classes and property

---
+++ BlogLab.Models/Account/ApplicationUserCreate.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogLab.Models.Account
+{
+    public class ApplicationUserCreate : ApplicationUserLogin
+    {
+        [MinLength(10, ErrorMessage = "Must be at least 10-30 characters")]
+        [MaxLength(30, ErrorMessage = "Must be at least 10-30 characters")]
+        public string Fullname { get; set; }
+
+
+        [Required(ErrorMessage = "Email is required")]
+        [MaxLength(30, ErrorMessage = "Can be at most 30 characters")]
+        [EmailAddress(ErrorMessage = "Invalid email format")]
+        public string Email { get; set; }
+
+    }
+}
